600873a93a7753dd796d78d6e0c9418313e8b45a
Kamil Fudala <kamil.fudala44@gmail.com>
2025-10-28 18:18:51 +0100

    Remove unused global using.

--- Lab02/Task01/GlobalUsings.cs
+++ Lab02/Task01/GlobalUsings.cs
@@ -2,6 +2,5 @@ global using System;
 global using System.Collections.Generic;
 global using System.Globalization;
 global using System.IO;
-global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
