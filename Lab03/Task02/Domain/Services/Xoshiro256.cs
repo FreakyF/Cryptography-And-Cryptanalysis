f8dd449b1330ac714d840c23db24bd6b7a72fec0
Kamil Fudala <kamil.fudala44@gmail.com>
2025-11-19 17:03:38 +0100

    Move rng and model to separate files.

---
+++ Lab03/Task02/Domain/Services/Xoshiro256.cs
@@ -0,0 +1,76 @@
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Task02.Domain.Services;
+
+public struct Xoshiro256
+{
+    private ulong _s0, _s1, _s2, _s3;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static ulong RotL(ulong x, int k)
+    {
+        return BitOperations.RotateLeft(x, k);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static ulong SplitMix64(ref ulong x)
+    {
+        x += 0x9E3779B97F4A7C15ul;
+        var z = x;
+        z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9ul;
+        z = (z ^ (z >> 27)) * 0x94D049BB133111EBul;
+        return z ^ (z >> 31);
+    }
+
+    public Xoshiro256(ulong seed)
+    {
+        _s0 = _s1 = _s2 = _s3 = 0;
+        var sm = seed;
+        _s0 = SplitMix64(ref sm);
+        _s1 = SplitMix64(ref sm);
+        _s2 = SplitMix64(ref sm);
+        _s3 = SplitMix64(ref sm);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private ulong Next64()
+    {
+        var s0 = _s0;
+        var s1 = _s1;
+        var s2 = _s2;
+        var s3 = _s3;
+
+        var result = RotL(s1 * 5, 7) * 9;
+
+        var t = s1 << 17;
+
+        s2 ^= s0;
+        s3 ^= s1;
+        s1 ^= s2;
+        s0 ^= s3;
+
+        s2 ^= t;
+        s3 = RotL(s3, 45);
+
+        _s0 = s0;
+        _s1 = s1;
+        _s2 = s2;
+        _s3 = s3;
+        return result;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int NextInt(int exclusiveMax)
+    {
+        var r = Next64();
+        var prod = (UInt128)r * (uint)exclusiveMax;
+        return (int)(prod >> 64);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public double NextDouble()
+    {
+        return (Next64() >> 11) * (1.0 / (1ul << 53));
+    }
+}
